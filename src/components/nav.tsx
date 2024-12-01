c620d272b7d1e16368a2f6a48b44c5ee6bbf57bb
Cyandev <unixzii@gmail.com>
2024-12-01 22:55:05 +0800

    Add adaptive nav menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,14 @@
 "use client";
 
-import { Fragment, useLayoutEffect, useRef, useState } from "react";
+import {
+  Fragment,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import Link from "next/link";
+import { useSpring, animated } from "@react-spring/web";
 import invariant from "invariant";
 import { selectClass } from "@/utils";
 import { Icon } from "@/components/icon";
@@ -9,14 +16,17 @@ import { ReadableArea } from "./adaptive-containers";
 import { RevealHighlightPlatter, useRevealHighlight } from "./reveal-highlight";
 import me from "../../data/me.json";
 
+import "./nav.css";
+
 interface NavLinkProps {
   title: string;
   icon?: string;
   href: string;
+  mobile?: boolean;
 }
 
 function NavLink(props: NavLinkProps) {
-  const { title, icon, href } = props;
+  const { title, icon, href, mobile } = props;
 
   const { targetProps } = useRevealHighlight({
     insetWidth: !!icon ? 0 : 2,
@@ -25,33 +35,107 @@ function NavLink(props: NavLinkProps) {
 
   return (
     <Link
-      className="px-3 hover:text-foreground transition-colors duration-200"
-      href={props.href}
+      className={selectClass(
+        { "px-3": !mobile },
+        "hover:text-foreground transition-colors duration-200"
+      )}
+      href={href}
       onMouseEnter={targetProps.onMouseEnter}
       onMouseLeave={targetProps.onMouseLeave}
       onMouseDown={targetProps.onMouseDown}
       onMouseUp={targetProps.onMouseUp}
       aria-label={title}
     >
-      {icon ? <Icon icon={icon as any} /> : title}
+      {icon ? (
+        <Icon icon={icon as any} size={mobile ? "lg" : undefined} />
+      ) : (
+        title
+      )}
     </Link>
   );
 }
 
-function NavLinks() {
+interface NavLinksProps {
+  className?: string;
+  mobile?: boolean;
+}
+
+function NavLinks(props: NavLinksProps) {
+  const { mobile, className } = props;
   return (
-    <div className="flex flex-1 font-light text-foreground-secondary">
-      <NavLink title="Notes" href="/" />
-      <NavLink title="About Me" href="/" />
+    <div
+      className={selectClass(
+        {
+          "hidden sm:flex flex-1": !mobile,
+          "flex flex-col gap-4 pb-4": mobile,
+        },
+        "font-light text-foreground-secondary" +
+          (className ? ` ${className}` : "")
+      )}
+    >
+      <NavLink title="Notes" href="/" mobile={mobile} />
+      <NavLink title="About Me" href="/" mobile={mobile} />
+      <div className={mobile ? "hidden" : "flex-1"} />
+      <div className={mobile ? "flex gap-6" : "contents"}>
+        {me.links.map((link) => (
+          <NavLink
+            key={link.title}
+            title={link.title}
+            icon={link.icon}
+            href={link.url}
+            mobile={mobile}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+interface MobileMenuProps {
+  onExpandChanged(height?: number): void;
+}
+
+function MobileMenu(props: MobileMenuProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  const menuInnerElementRef = useRef<HTMLDivElement>(null);
+
+  function toggleExpanded() {
+    setExpanded(!expanded);
+    let targetHeight = 0;
+    if (!expanded) {
+      targetHeight = menuInnerElementRef.current!!.clientHeight;
+    }
+    props.onExpandChanged(targetHeight);
+  }
+
+  return (
+    <div className="flex sm:hidden flex-1">
       <div className="flex-1" />
-      {me.links.map((link) => (
-        <NavLink
-          key={link.title}
-          title={link.title}
-          icon={link.icon}
-          href={link.url}
+      <button className="p-[4px]" onClick={toggleExpanded}>
+        <div
+          className={selectClass(
+            { "cyan-mobile-menu-expanded": expanded },
+            "cyan-mobile-menu-icon"
+          )}
         />
-      ))}
+      </button>
+      <div
+        ref={menuInnerElementRef}
+        className="absolute left-0 top-[52px] w-full"
+      >
+        <ReadableArea>
+          <NavLinks
+            className={selectClass(
+              {
+                "opacity-0": !expanded,
+              },
+              "transition-opacity duration-200"
+            )}
+            mobile
+          />
+        </ReadableArea>
+      </div>
     </div>
   );
 }
@@ -76,23 +160,46 @@ export function NavBar() {
     };
   }, [scrollDetectorElementRef, setHairlineVisible]);
 
+  const [expanded, setExpanded] = useState(false);
+  const [springValues, springApi] = useSpring(() => {
+    return {
+      height: 52,
+    };
+  }, []);
+  const handleExpandChanged = useCallback(
+    (height?: number) => {
+      setExpanded(height ? true : false);
+      springApi.start({
+        to: { height: 52 + (height ?? 0) },
+        config: {
+          tension: 400,
+          friction: 40,
+        },
+      });
+    },
+    [setExpanded, springApi]
+  );
+
   return (
     <Fragment>
-      <nav
-        className={selectClass({
-          "fixed top-0 w-full py-4 z-50 bg-backdrop-tint backdrop-blur border-b transition-colors duration-500":
-            true,
-          "border-border": hairlineVisible,
-          "border-transparent": !hairlineVisible,
-        })}
+      <animated.nav
+        className={selectClass(
+          {
+            "border-border": hairlineVisible || expanded,
+            "border-transparent": !(hairlineVisible || expanded),
+          },
+          "fixed top-0 w-full z-50 bg-backdrop-tint backdrop-blur border-b transition-colors duration-500 overflow-hidden"
+        )}
+        style={{ height: springValues.height }}
       >
-        <ReadableArea className="flex">
+        <ReadableArea className="flex items-center h-[52px]">
           <span className="font-bold cursor-default mr-3">Cyandev</span>
           <RevealHighlightPlatter>
             <NavLinks />
           </RevealHighlightPlatter>
+          <MobileMenu onExpandChanged={handleExpandChanged} />
         </ReadableArea>
-      </nav>
+      </animated.nav>
       <div className="h-16" ref={scrollDetectorElementRef} />
     </Fragment>
   );
