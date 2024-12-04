885538a53958574b867fa8693709d3d9dcdd622b
Cyandev <unixzii@gmail.com>
2024-12-04 21:56:56 +0800

    Fix viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { PropsWithChildren } from "react";
+import type { Viewport } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter, Noto_Serif, Noto_Serif_SC, Geist_Mono } from "next/font/google";
 import { config as faConfig } from "@fortawesome/fontawesome-svg-core";
@@ -36,23 +37,19 @@ const fontClassNames = [
 // which may cause layout shifts.
 faConfig.autoAddCss = false;
 
+export const viewport: Viewport = {
+  themeColor: [
+    { color: "#78b3ce", media: "(prefers-color-scheme: light)" },
+    { color: "#80b8c6", media: "(prefers-color-scheme: dark)" },
+  ],
+  viewportFit: "cover",
+};
+
 export const metadata = buildMetadata({});
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: light)"
-          content="#78b3ce"
-        />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#80b8c6"
-        />
-      </head>
       <body className={fontClassNames}>
         <div className="relative min-h-screen">
           <GrainTextureBackground />
