d97b3fc944bf5c13fd9923f1477428a02c297d80
Cyandev <unixzii@gmail.com>
2025-08-22 11:08:14 +0800

    Fix type issues

--- src/app/rss/renderHelper.ts
+++ src/app/rss/renderHelper.ts
@@ -1,8 +1,8 @@
 import { jsx } from "react/jsx-runtime";
 import type { ComponentProps } from "react";
-import type { PostModule } from "post-catalog-loader!";
+import type { RefinedPostModule } from "@/data/posts";
 
-export async function renderPostToString(module: PostModule) {
+export async function renderPostToString(module: RefinedPostModule) {
   const ReactDOMServer = await import("react-dom/server");
   return ReactDOMServer.renderToString(
     jsx(module.MDXContent, {
